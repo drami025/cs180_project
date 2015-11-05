87f0af30349cbed75c3b20f867a788aa4b283b51
jpere043 <jpere043@ucr.edu>
2015-11-05 06:47:08 -0800

    Julie changed preferences and added comonent and color picker java files

---
+++ app/src/main/java/com/cs180/ucrtinder/ucrtinder/ui/Component.java
@@ -0,0 +1,8 @@
+package com.cs180.ucrtinder.ucrtinder.ui;
+
+/**
+ * Created by Owner on 11/5/2015.
+ */
+public enum Component {
+    BAR_COLOR, CONNECTING_LINE_COLOR, THUMB_COLOR_NORMAL, THUMB_COLOR_PRESSED,
+}
